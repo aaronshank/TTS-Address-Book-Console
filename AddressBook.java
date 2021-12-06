cc3662f33241000393df86284f9c2c8d22164b4c
Aaron Shank <47933546+aaronshank@users.noreply.github.com>
2021-12-06 11:36:28 -0500

    initializing

---
+++ AddressBook.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+    List<Entry> entries = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("1) Add an entry");
+            System.out.println("2) Remove an entry");
+            System.out.println("3) Search for a specific entry");
+            System.out.println("4) Print Address Book");
+            System.out.println("5) Delete Book");
+            System.out.println("6) Quit");
+            System.out.print("\nPlease choose what you'd like to do with the database: ");
+            int response = Integer.parseInt(input.nextLine());
+            switch (response) {
+                case 1:
+                    addEntry();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    isRunning = false;
+            }
+        }
+        input.close();
+    }
+
+    static void addEntry() {
+        Scanner input = new Scanner(System.in);
+        Entry entry = new Entry();
+        System.out.print("First Name: ");
+        entry.setFirstName(input.nextLine());
+        System.out.print("Last Name: ");
+        entry.setLastName(input.nextLine());
+        System.out.print("Phone Number: ");
+        entry.setPhoneNumber(input.nextLine());
+        System.out.print("Email Address: ");
+        entry.setEmailAddress(input.nextLine());
+        System.out.println("Added new entry!");
+        input.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Address Book
+
+The application should be able to do the following:
+* Present the user with a list of options to interact with the database and take in their input
+* Allow the user to perform the following actions:
+    * Add an entry
+    * Remove and entry
+    * Search for a specific entry
+    * Print the contents of the address book
+    * Delete the contents of the address book
+    * Quit the program
